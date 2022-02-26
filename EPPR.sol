df791426ab26f850ea422a3724d12e8d8691255c
EPPR LXPS <67383945+eppr-lxps@users.noreply.github.com>
2022-02-26 10:05:03 -0600

    Update EPPR.sol

--- EPPR.sol
+++ EPPR.sol
@@ -7,12 +7,13 @@ contract EPPR {
     address owner;
     constructor () {
         // Guarda un valor inicial en la variable "owner" (Al hacer deploy a este contrato)
-        owner = msg.sender;
+        owner = 0x0000000000000000000000000000000000000000;
     }
     // Función publica: Revisa si la cuenta actual es el dueño del contrato.
     function isOwner() 
         public
         view
+        onlyOwner() // Aquí se define el uso del modificador.
         returns (bool) 
     {
         if(owner == msg.sender){
@@ -20,5 +21,10 @@ contract EPPR {
         }else{
             return false;
         }
+    }
+    // Modifier: Permite cambiar el comportamiento de otra función. 
+    modifier onlyOwner {
+        require(msg.sender == owner, "Error: Not the Owner");
+        _; // Este "_;" se llama "merge wildcard" y sirve para unir las dos funciones en cadena.
     }
 }
