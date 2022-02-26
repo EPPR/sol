056465753afed20e593bef1df2a43c4081b0e4fe
EPPR LXPS <67383945+eppr-lxps@users.noreply.github.com>
2022-02-26 10:22:26 -0600

    Update EPPR.sol

--- EPPR.sol
+++ EPPR.sol
@@ -7,13 +7,12 @@ contract EPPR {
     address owner;
     constructor () {
         // Guarda un valor inicial en la variable "owner" (Al hacer deploy a este contrato)
-        owner = 0x0000000000000000000000000000000000000000;
+        owner = msg.sender;
     }
-    // Función publica: Revisa si la cuenta actual es el dueño del contrato.
+    // Función pública: Revisa si la cuenta actual es el dueño del contrato.
     function isOwner() 
         public
         view
-        onlyOwner() // Aquí se define el uso del modificador.
         returns (bool) 
     {
         if(owner == msg.sender){
@@ -22,9 +21,32 @@ contract EPPR {
             return false;
         }
     }
+    // Función pública: Transfiere este contracto a otra cuenta. (Solo los dueños pueden transferir el contrato y no puedes transferirlo a ti mismo)
+    function transferContract(address _to) 
+        public
+        onlyOwner()
+        throwIfIsOwnerBytes32(_to)
+        returns (bool) 
+    {
+        owner = _to;
+        return true;
+    }
+    // Función pública: No tiene mods, cualquier persona puede leer esta info, imprime la dirección actual definida como "owner"
+    function getOwner() 
+        public
+        view
+        returns (address) 
+    {
+        return owner;
+    }
     // Modifier: Permite cambiar el comportamiento de otra función. 
     modifier onlyOwner {
-        require(msg.sender == owner, "Error: Not the Owner");
+        require(msg.sender == owner, "You Are Not The Owner.");
         _; // Este "_;" se llama "merge wildcard" y sirve para unir las dos funciones en cadena.
+    }
+    // Mod para cancelar la transacción en caso que la cuenta sea el dueño de este contrato.
+    modifier throwIfIsOwnerBytes32(address _id) {
+        require(_id != owner, "Invalid transfer destination.");
+        _;
     }
 }
